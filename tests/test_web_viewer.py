3afd606dcde538b9ea1def9aaa3abfea248dae57
yg-ht <x@yg.ht>
2026-07-13 22:06:00 +0100

    Add mesh graph max paths filter

--- tests/test_web_viewer.py
+++ tests/test_web_viewer.py
@@ -250,9 +250,12 @@ class TestPageRoutes:
         assert 'id="filter-path-from"' in html
         assert 'id="filter-path-to"' in html
         assert 'id="filter-path-max-hops"' in html
+        assert 'id="filter-path-max-results"' in html
         assert 'value="5"' in html
-        assert 'MAX_PATH_FILTER_HOPS = 8' in html
-        assert 'MAX_PATH_FILTER_RESULTS = 100' in html
+        assert 'value="100"' in html
+        assert 'id="filter-path-max-hops" min="1" value="5"' in html
+        assert 'id="filter-path-max-results" min="1" value="100"' in html
+        assert 'DEFAULT_PATH_FILTER_MAX_RESULTS = 100' in html
         assert 'function enumerateAllSimplePathsBetweenNodes' in html
         assert 'function applyPathBetweenNodesFilter' in html
         assert 'function clearPathBetweenNodesFilter' in html
